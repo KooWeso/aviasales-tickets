71c88187fc236896521c604c6c2f3c28f15b001d
KooWeso <koowa360@gmail.com>
2024-08-09 15:45:28 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>1</div>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+    </>
+  )
+}
+
+export default App
